e7ea3c91031fb988bf4834fbf7ddb31c2241ba5c
Thiago Silva Antenor <thiagoantenor31@gmail.com>
2022-10-07 19:20:41 -0300

    First commit

---
+++ src/data/movies.js
@@ -0,0 +1,22 @@
+export const movies = [
+  {
+    id: 1,
+    title: 'The Matrix',
+    year: 1999,
+    director: 'Lana Wachowski',
+    ranting: 'R',
+    runtime: 136,
+    poster:
+      'https://img.elo7.com.br/product/zoom/2679A17/big-poster-filme-matrix-lo02-tamanho-90x60-cm-poster-de-filme.jpg'
+  },
+  {
+    id: 2,
+    title: 'The Lord of the Rings',
+    year: 2001,
+    director: 'Peter Jackson',
+    ranting: 'PG-13',
+    runtime: 178,
+    poster:
+      'https://alternativemovieposters.com/wp-content/uploads/2021/01/RuizBurgos_FellowshipoftheRing.jpg'
+  }
+]
